33972d9d357879a2ed9447b32d3d28823fa6ebeb
Sai Patanjali <patanjali1729@gmail.com>
2018-01-23 13:31:19 +0530

    Initial commit - created repo - initialized git

---
+++ app/src/main/java/com/example/sai/prviewer/PRViewer.java
@@ -0,0 +1,11 @@
+package com.example.sai.prviewer;
+
+import android.app.Application;
+
+/**
+ * Created by sai on 23/01/18.
+ */
+
+public class PRViewer extends Application {
+
+}
---
+++ app/src/main/java/com/example/sai/prviewer/view/RepositoryActivity.java
@@ -0,0 +1,15 @@
+package com.example.sai.prviewer.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sai.prviewer.R;
+
+public class RepositoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository);
+    }
+}
